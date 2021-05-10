20c532d3f7c60845f7b9435eac207f1db494892e
Zhyrgalbek-GeekTech <zhekchyn@gmail.com>
2021-05-10 23:50:29 +0300

    Initial commit

---
+++ src/com/geektech/Boss.java
@@ -0,0 +1,31 @@
+package com.geektech;
+
+public class Boss {
+    private int health;
+    private int damage;
+    private String typeOfBlock;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getTypeOfBlock() {
+        return typeOfBlock;
+    }
+
+    public void setTypeOfBlock(String typeOfBlock) {
+        this.typeOfBlock = typeOfBlock;
+    }
+}
---
+++ src/com/geektech/Main.java
@@ -0,0 +1,44 @@
+package com.geektech;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    	// Main Home Work:
+	Boss Baby = new Boss();
+	Baby.setHealth(500);
+	Baby.setDamage(150);
+	Baby.setTypeOfBlock("Super Block");
+
+        System.out.println(
+        		"Baby boss health: " + Baby.getHealth() +
+				" | Baby boss damage: " + Baby.getDamage() + " | " +
+						"Baby boss type of block: " + Baby.getTypeOfBlock());
+
+
+		}
+
+
+
+
+
+
+	// Additional Home Work: failed
+//	public static Hero[] createHeroes() {
+//		Hero Marat = new Hero(600, 250);
+//		Hero Aidar = new Hero(550, 125, "Laser");
+//		Hero Zhyrgal = new Hero(500, 120, "Muscle");
+//
+//		Hero[] heroes = {Marat, Aidar, Zhyrgal};
+
+
+
+
+
+
+
+
+
+
+}
+
